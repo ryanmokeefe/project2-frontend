e1fc08adc1b902b1fed6cd7385abfa13f24b1a02
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-04-04 18:39:53 -0400

    deploying mvp to surge

--- src/Utils.js
+++ src/Utils.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export function queryResources (query) {
   
-  // const url = 'https://additional-resources.herokuapp.com/'
-  const url = 'http://localhost:4000/resources'
+  const url = 'https://additional-resources.herokuapp.com/'
+  // const url = 'http://localhost:4000/resources'
 
   return axios.get(url)
    
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-// const CLIENT_URL = 'https://additional-resources.herokuapp.com/'
- const CLIENT_URL = 'http://localhost:4000/resources'
+const CLIENT_URL = 'https://additional-resources.herokuapp.com/'
+//  const CLIENT_URL = 'http://localhost:4000/resources'
 
  export default CLIENT_URL
 
