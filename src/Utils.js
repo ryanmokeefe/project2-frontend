d17bbe4ae0c49cd658596bc8bce7b51138197cfe
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-04-05 13:00:21 -0400

    build version - fixed db

--- src/Utils.js
+++ src/Utils.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function queryResources (query) {
   
-  const url = 'https://additional-resources.herokuapp.com/'
+  const url = 'https://additional-resources.herokuapp.com/resources'
   // const url = 'http://localhost:4000/resources'
 
   return axios.get(url)
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const CLIENT_URL = 'https://additional-resources.herokuapp.com/'
+const CLIENT_URL = 'https://additional-resources.herokuapp.com/resources'
 //  const CLIENT_URL = 'http://localhost:4000/resources'
 
  export default CLIENT_URL
